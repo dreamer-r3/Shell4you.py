a96e0cc8b0d84fd45a3dacb23ce65ffa7c03e4dc
0x1v4n <131263019+0x1v4n@users.noreply.github.com>
2024-06-15 20:02:16 +0200

    Update Shell2You.py

--- Shell2You.py
+++ Shell2You.py
@@ -15,7 +15,7 @@ yellow = "\033[1;33m"
 def menu():
     print(red + '|-----------------------------|' + reset)
     print(red + '|   Welcome to Shell2you!     |' + reset)
-    print(red + '|       Dev By Ivan           |' + reset)
+    print(red + '|       Dev By 0x1v4n         |' + reset)
     print(red + '|-----------------------------|' + reset + "\n")
 
 def validate_ip(ip):
